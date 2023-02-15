a3bcab9b6f7401efa0c6c2084b5e8bf7d8ee6f5e
tomisola1 <tomisoladebby@gmail.com>
2023-02-15 23:44:54 +0100

    navbar and home done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <SocialLinks />
+    </div>
+  );
 }
 
 export default App;
